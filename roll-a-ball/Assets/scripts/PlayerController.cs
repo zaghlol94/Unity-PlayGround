709f273a7cfba5599c2eb0428cba5cd8056862d3
zaghlol94 <zaghlollight@gmail.com>
2017-10-07 18:53:21 +0200

    Initial commit

---
+++ roll-a-ball/Assets/scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public Text CountText;
+    public Text WinText;
+
+    private int count;
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCount();
+        WinText.text = "";
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 mouvment = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(mouvment*speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCount();
+            if (count>=9)
+            {
+                WinText.text = "YOU WIN!!";
+            }
+        }
+
+   }
+
+    void SetCount()
+    {
+        CountText.text = "count:" + count.ToString();
+
+    }
+}
